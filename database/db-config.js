03cd98c6b6449f9e2bb347a281cf3b2770ae2252
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-09-22 16:52:16 -0400

    Switch to production

--- database/db-config.js
+++ database/db-config.js
@@ -2,7 +2,7 @@ const environment = process.env.NODE_ENV || 'development';
 
 const knex = require('knex');
 
-const knexConfig = require('../knexfile');
+const knexConfig = require('../knexfile.js');
 
 // module.exports = knex(knexConfig.production);
-module.exports = knex(knexConfig[environment]);
+module.exports = knex(knexConfig.production);
--- knexfile.js
+++ knexfile.js
@@ -10,5 +10,16 @@ module.exports = {
         seeds: {
             directory: './database/seeds'
         }
+    },
+    production: {
+        client: 'pg',
+        connection: process.env.DATABASE_URL,
+        useNullAsDefault: true,
+        migrations: {
+            directory: './database/migrations'
+        },
+        seeds: {
+            directory: './database/seeds'
+        }
     }
 };
