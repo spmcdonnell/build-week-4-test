4f675f5ffe54912c7cc839c908e695dee1424c86
Sean McDonnell <seanmcdonnell@Seans-MacBook-Pro.local>
2019-10-18 15:34:12 -0700

    _

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -12,17 +12,3 @@ function getAll() {
 function getUser(user_id) {
     return db('users').where({ id: user_id });
 }
-const db = require('../../database/db-config.js');
-
-module.exports = {
-    getAll,
-    getUser
-};
-
-function getAll() {
-    return db('users');
-}
-
-function getUser(user_id) {
-    return db('users').where({ id: user_id });
-}
